98e5131ff3e43f159305a35adaed9065cfdba20e
KPAMsar <kpamsarshija@gmail.com>
2023-05-01 07:19:04 +0100

    minor clean up

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Products from './Products/addProducts';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 // import {AllProducts} from './Products/getProducts'
